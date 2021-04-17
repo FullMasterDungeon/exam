1ed6f0b4cc91fdeda2d08a643bdabf395eb093a9
FullMasterDungeon <81224815+FullMasterDungeon@users.noreply.github.com>
2021-04-17 22:28:46 +0600

    Update terminator.py

--- terminator.py
+++ terminator.py
@@ -5,12 +5,12 @@ fist = ['e', 'y', 'u', 'i', 'o', 'a']
 bot = telebot.TeleBot('1751556596:AAGYxFmWp89tI6YETxtr8DDTd9LvXUd6DOw')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    count = 0
     atext = message.text.lower()
     for i in atext:
-        for b in fist:
-            if i == b:
-                count += 1
-    bot.send_message(message.chat.id, count)
-    сount = 0
+        for a, b in fist.items():
+            if i == a:
+                fist[i] += 1
+    for i, b in fist.items():
+        bot.send_message(message.chat.id, f"{i} повторяется {b} раз")
+        fist[i] = 0
 bot.polling()
